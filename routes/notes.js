eba3704b73106be90c6accab5df2776909352550
Origin 42 <daveplum1991@gmail.com>
2022-04-30 12:49:08 +0800

    updated post

--- routes/notes.js
+++ routes/notes.js
@@ -1,9 +1,22 @@
 const notes = require('express').Router();
 const { v4: uuidv4 } = require('uuid');
-const { readFromFile } = require('../helpers/fsUtils');
+const { readFromFile, readAndAppend } = require('../helpers/fsUtils');
 
 notes.get('/', (req, res) => {
     readFromFile('./db/notes.json').then((data) => res.json(JSON.parse(data)));
+});
+notes.post('/', (req, res) => {
+    const { title, text } = req.body;
+
+    const payload = {
+        id: uuidv4(),
+        title: title,
+        text: text
+    }
+
+    readAndAppend(payload, './db/notes.json');
+
+    res.status(201).json(payload);
 });
 
 module.exports = notes;
